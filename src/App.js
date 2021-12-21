d08be8fbdb16176cf912356d0199c28820926c99
blueisis <pureun96@naver.com>
2021-12-22 00:26:46 +0900

    step5 컴포넌트 Panal 반복처리 개선

--- src/App.js
+++ src/App.js
@@ -4,11 +4,6 @@ import Footer from './components/Footer.js';
 import Panal from './components/Panal.js';
 // import img1 from './img/blizzards.jpg';
 
-
-const path = process.env.PUBLIC_URL;
-const arr = ['Blizzards','Calm','Dusty_Road','Escape','Payday','Retreat','Seasonal','Vespers'];
-
-
 function App() {
   return (
     <div className="App">
@@ -18,11 +13,7 @@ function App() {
         <Menu />
 
         <section>
-          {
-            arr.map((data,index)=>{
-              return <Panal />
-            })
-          }
+          <Panal />
         </section>
 
         <Footer />
--- src/components/Panal.js
+++ src/components/Panal.js
@@ -1,13 +1,25 @@
 function Panal(){
+    const path = process.env.PUBLIC_URL;
+    const arr = ['Blizzards','Calm','Dusty_Road','Escape','Payday','Retreat','Seasonal','Vespers'];
+    const deg = 360/arr.length;
+    
     return(
-        <article key={index}>
-            <div className="inner">
-                <div className="pic">
-                    <img src={`${path}/img/${data}.jpg`} />
-                </div>
-                <h2>{data}</h2>
-            </div>
-        </article>
+        <>
+            {
+            arr.map((data,index)=>{
+                    return(
+                        <article key={index} style={{transform:`rotate(${deg*index}deg) translateY(-100vh)`}}>
+                            <div className="inner">
+                                <div className="pic">
+                                    <img src={`${path}/img/${data}.jpg`} />
+                                </div>
+                                <h2>{data}</h2>
+                            </div>
+                        </article>
+                    )
+                })
+            }
+        </>
     )
 }
 
