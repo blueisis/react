a70d37df31a458ee04d67f26fe4927e323d3f699
blueisis <pureun96@naver.com>
2021-12-21 19:04:25 +0900

    step2-1 사스 header/footer 작성

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import './css/style.css';
 function App() {
   return (
     <div className="App">
-      <article></article>
+      <figure>
+        <h1>
+          <strong>DCODELAB</strong><br />
+          <span>UX/UI DESIGN & DEVELOPMENT</span>
+        </h1>
+
+        <a href="#" className="menu">
+          <i className="fas fa-bars"></i>
+        </a>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      </figure>
     </div>
   );
 }
